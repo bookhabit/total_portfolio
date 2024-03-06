90cf3ef98b48a37d045477ede522ed9d02d69540
bookhabit <qnrgoqlt@naver.com>
2024-03-06 19:58:24 +0900

    플래시버드(ball) 게임완성

---
+++ GameCoding/FlappyBall/index.js
@@ -0,0 +1,54 @@
+var block = document.getElementById("block")
+var hole = document.getElementById("hole")
+var character = document.getElementById("character")
+
+var jumping = 0;
+var counter = 0;
+
+hole.addEventListener("animationiteration",()=>{
+    var random = -((Math.random()*300)+150);
+    hole.style.top = random +"px";
+    counter++;
+})
+
+setInterval(()=>{
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    if(jumping == 0){
+        character.style.top = (characterTop+3)+"px";
+    }
+
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"))
+    var cTop = - (500-characterTop);
+
+    // 게임종료
+    if((characterTop>480) || ((blockLeft <20) && (blockLeft > -50)) && ( (cTop<holeTop) || (cTop>holeTop+130)) ){
+        if(counter == 0 ){
+            alert("game over. Score: "+(counter));
+        }else{
+            alert("game over. Score: "+(counter-1));
+        }
+        
+        // 화면 위에 스코어 표시해주기
+        character.style.top = 100 + "px"
+        counter = 0;
+    }
+},10)
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(() => {
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+        // 점프 높이 : default 4
+        if((characterTop>6) && (jumpCount<15)){
+            character.style.top = (characterTop-4) + "px"
+        }
+        if(jumpCount > 20){
+            clearInterval(jumpInterval)
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++
+    }, 10);
+}
