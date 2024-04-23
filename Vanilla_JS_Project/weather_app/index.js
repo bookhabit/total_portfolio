fee3efd5a283b8fadbdfb6c8a31d7492a034feb1
bookhabit <qnrgoqlt@naver.com>
2024-04-24 00:26:52 +0900

    날씨앱 완성

--- Vanilla_JS_Project/weather_app/index.js
+++ Vanilla_JS_Project/weather_app/index.js
@@ -10,6 +10,7 @@ search.addEventListener("click",()=>{
     console.log('city값',city)
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`).then(response=>response.json()).then(json=>{
+        console.log('응답데이터 json',json)
         if(json.cod==='404'){
             console.log('404페이지')
             container.style.height = '400px';
@@ -22,6 +23,45 @@ search.addEventListener("click",()=>{
         error404.classList.remove('fadeIn')
 
         const image = document.querySelector('.weather-box img')
+        const temperature = document.querySelector(".weather-box .temperature");
+        const description = document.querySelector('.weather-box .description')
+        const humidity = document.querySelector('.weather-details .humidity span')
+        const wind = document.querySelector('.weather-details .wind span')
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+
+            case 'Haze':
+                image.src = 'images/mist.png';
+                break;
+
+            default:
+                image.src = '';
+        }
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '590px';
     })
 
 })
