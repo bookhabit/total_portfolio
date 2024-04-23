3b9e80fe0eab9f6b391a4f954acdb3e0094bb22e
bookhabit <qnrgoqlt@naver.com>
2024-04-23 18:28:04 +0900

    테트리스 게임

---
+++ Vanilla_JS_Project/weather_app/index.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container')
+const search = document.querySelector('.search-box button')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetails = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found')
+
+search.addEventListener("click",()=>{
+    const APIkey = 'f94c58fb695ce20e76a14647d27a584a'
+    const city = document.querySelector('.search-box input').value;
+    console.log('city값',city)
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`).then(response=>response.json()).then(json=>{
+        if(json.cod==='404'){
+            console.log('404페이지')
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+        }
+        error404.style.display = 'none';
+        error404.classList.remove('fadeIn')
+
+        const image = document.querySelector('.weather-box img')
+    })
+
+})
