0e9cd96bfcbd30324a7e033a060c416181ba5cc8
bookhabit <qnrgoqlt@naver.com>
2024-07-10 23:09:43 +0900

    퍼즐게임 - html 및 css 작업

---
+++ GameCoding/puzzle_game/main.js
@@ -0,0 +1,7 @@
+const imgContainer = document.querySelector(".image-container")
+const startButton = document.querySelector(".start-button")
+const gameText = document.querySelector(".game-text")
+const playTime = document.querySelector(".play-time")
+
+const li = document.querySelectorAll(".image-container li")
+
